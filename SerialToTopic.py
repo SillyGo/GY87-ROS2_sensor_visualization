c8175c7cecbf34bca891e5b2a70c73abe296d936
Arthur Calabria Guerra <163882421+SillyGo@users.noreply.github.com>
2026-02-27 12:03:18 -0300

    dev - mudando a formatação do dado serial

--- SerialToTopic.py
+++ SerialToTopic.py
@@ -10,71 +10,74 @@ PORT_NAME = "/dev/ttyUSB0"
 BAUD_RATE = 115200
 
 ser = serial.Serial(PORT_NAME, BAUD_RATE, timeout=1) 
-time.sleep(2)
+time.sleep(1) #deixa o serial ir printando para não ter problema possível de ler linha vazia
 
 class ImuPublisher(Node):
     def __init__(self):
         super().__init__('imu_sensor_publisher')
         self.imupub = self.create_publisher(Imu,'/imu_data_raw',10)
         self.magpub = self.create_publisher(MagneticField,'/mag_data_raw',10)
-        self.timer = self.create_timer(0.1, self.TimerCallback)
+        self.timer = self.create_timer(0.01, self.TimerCallback)
 
     def TimerCallback(self):
         self.get_logger().info("PUBLICANDO DADO DA IMU")
+        PUB = True
 
         #1.extrai ax,ay,az, gx,gy,gz, mx,my,mz da IMU com pyserial e arduino
         
-        accdata = ser.readline()
-        accdata = accdata.decode('utf-8').strip()
+        try:
+            data = ser.readline()
+            data = data.decode('utf-8').strip()
 
-        gyrodata = ser.readline()
-        gyrodata = gyrodata.decode('utf-8').strip()
+            data = data.split(" ")
 
-        magdata = ser.readline()
-        magdata = magdata.decode('utf-8').strip()
+            ax, ay, az = float(data[0]), float(data[1]), float(data[2])
+            gx, gy, gz = float(data[3]), float(data[4]), float(data[5])
+            mx, my, mz = float(data[6]), float(data[7]), float(data[8])
 
-        acc = accdata.split(" ")
-        gyr = gyrodata.split(" ")
-        mag = magdata.split(" ")
+            print(f"a: {ax,ay,az}")
+            print(f"g: {gx,gy,gz}")
+            print(f"m: {mx,my,mz}")
+            
+        except:
+            print("não recebi dados")
+            PUB = False
 
-        ax, ay, az = float(acc[0]), float(acc[1]), float(acc[2])
-        gx, gy, gz = float(gyr[0]), float(gyr[1]), float(gyr[2])
-        mx, my, mz = float(mag[0]), float(mag[1]), float(mag[2])
-
-        #2.constroi a mensagem da IMU
-        msg = Imu()
-    
-        msg.header.stamp = self.get_clock().now().to_msg()
-        msg.header.frame_id = "imu_link"
-
-        msg.linear_acceleration.x = ax
-        msg.linear_acceleration.y = ay
-        msg.linear_acceleration.z = az
-
-        msg.angular_velocity.x = gx
-        msg.angular_velocity.y = gy
-        msg.angular_velocity.z = gz
-
-        msg.orientation_covariance[0] = -1
+        if PUB:
+            #2.constroi a mensagem da IMU
+            msg = Imu()
         
-        #3.publica a mensagem da IMU
+            msg.header.stamp = self.get_clock().now().to_msg()
+            msg.header.frame_id = "imu_link"
 
-        self.imupub.publish(msg)
-        
-        #4. constroi a mensagem do mag
+            msg.linear_acceleration.x = ax
+            msg.linear_acceleration.y = ay
+            msg.linear_acceleration.z = az
 
-        msg = MagneticField()
+            msg.angular_velocity.x = gx
+            msg.angular_velocity.y = gy
+            msg.angular_velocity.z = gz
 
-        msg.header.stamp = self.get_clock().now().to_msg()
-        msg.header.frame_id = "imu_link"
-        
-        msg.magnetic_field.x = mx
-        msg.magnetic_field.y = my
-        msg.magnetic_field.z = mz
+            msg.orientation_covariance[0] = -1
 
-        #5. publica a mensagem do mag
+            #3.publica a mensagem da IMU
 
-        self.magpub.publish(msg)
+            self.imupub.publish(msg)
+            
+            #4. constroi a mensagem do mag
+
+            msg = MagneticField()
+
+            msg.header.stamp = self.get_clock().now().to_msg()
+            msg.header.frame_id = "imu_link"
+            
+            msg.magnetic_field.x = mx
+            msg.magnetic_field.y = my
+            msg.magnetic_field.z = mz
+
+            #5. publica a mensagem do mag
+
+            self.magpub.publish(msg)
 
 def main():
     rclpy.init()
